4f5bcabea5b32cc415a303369f4f75c02be3d70f
Ramón Astudillo <ramon@astudillo.com>
2017-11-11 15:43:23 -0200

    Changed -l and -c arguments to ls and config

--- vimbox/__main__.py
+++ vimbox/__main__.py
@@ -17,33 +17,43 @@ def main(args=None):
         args = sys.argv[1:]
 
     # Argument-less options
-    if '-c' in args:
-        vim_edit_config()
-        exit()
-    elif '-l' in args:
-        print("")
-        for folder in folders():
-            print(folder)
-        print("")
-        exit()
+    if len(args) == 1:
 
-    # Rest of options and folder
-    file_path = None
-    force_creation = False
-    for option in args:
-        if option == '-f':
-            force_creation = True
-        elif file_path is not None:
+        # Single arguments
+        if args[1] == 'config':
+            vim_edit_config()
+        elif args[1] == 'ls':
+            print("")
+            for folder in folders():
+                print(folder)
+            print("")
+    else:
+
+        # Multiple arguments
+        file_path = None
+        force_creation = False
+        password = None
+        for option in args:
+            if option == '-f':
+                # Create new file
+                force_creation = True
+            elif option == '-e':
+                # Create new encripted file
+                force_creation = True
+                password = raw_input('Input file password: ')
+            elif option[0] == '/':
+                # Dropbox path
+                file_path = option
+            else:
+                vimbox_help()
+
+        # Check we got a file path
+        if file_path is None:
             vimbox_help()
-        else:
-            file_path = option
 
-    if file_path is None:
-        vimbox_help()
-
-    # Call edit utility
-    vimbox = VimBox()
-    vimbox.edit(file_path, force_creation=force_creation)
+        # Call edit utility
+        vimbox = VimBox()
+        vimbox.edit(file_path, force_creation=force_creation, password=password)
 
 if __name__ == "__main__":
     main()
