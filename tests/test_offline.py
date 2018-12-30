42061e8304e875c33106dfff35813cd3318a6c9d
Ramón Astudillo <ramon@astudillo.com>
2018-12-30 22:16:14 +0100

    Fix offline unit test double slash in paths

--- tests/test_offline.py
+++ tests/test_offline.py
@@ -9,13 +9,13 @@ from tools import run_in_environment, REMOTE_UNIT_TEST_FOLDER
 def test_main():
 
     # Creation
-    tmp_file = '%s/folder1/plain' % REMOTE_UNIT_TEST_FOLDER
+    tmp_file = '%sfolder1/plain' % REMOTE_UNIT_TEST_FOLDER
     main(['-f', tmp_file, 'This is some text'])
     # Check a local copy was created (if intended)
     assert os.path.isfile(get_local_file(tmp_file)), \
         "Creation of local file %s failed" % tmp_file
 
-    tmp_file = '%s/folder1/encrypted' % REMOTE_UNIT_TEST_FOLDER
+    tmp_file = '%sfolder1/encrypted' % REMOTE_UNIT_TEST_FOLDER
     worked = main(
         ['-e', tmp_file, 'This is some encrypted text'], password='dummy'
     )
@@ -24,7 +24,7 @@ def test_main():
         "Should not be able to remove files offline"
     assert not main(['cp', tmp_file, tmp_file + '2']), \
         "Should not be able to copy files offline"
-    tmp_folder = '%s/folder1/folder2/' % REMOTE_UNIT_TEST_FOLDER
+    tmp_folder = '%sfolder1/folder2/' % REMOTE_UNIT_TEST_FOLDER
     assert not main(['mkdir', tmp_folder]), \
         "Should not be able to make folder offline"
 
