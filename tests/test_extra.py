21a6cc385827e9ea05bd66738b12fa6e6b6298a3
Ramón Astudillo <ramon@astudillo.com>
2018-12-30 22:11:28 +0100

    text_extra.py working

--- tests/test_extra.py
+++ tests/test_extra.py
@@ -9,49 +9,46 @@ from tools import run_in_environment, green, REMOTE_UNIT_TEST_FOLDER
 
 def test_main():
 
-    main(['-f', '%s/folder1/plain' % REMOTE_UNIT_TEST_FOLDER, 'Cosa mariposa'])
-    assert main(
-        ['ls', '%s/folder1/plain' % REMOTE_UNIT_TEST_FOLDER], verbose=0
-    )
+    # Create plain file
+    plain_file = '%sfolder1/plain' % REMOTE_UNIT_TEST_FOLDER
+    assert main(['-f', plain_file, 'Cosa mariposa'])
+    assert main(['ls', plain_file], verbose=0), "Creating file failed"
 
-    main(['-e', '%s/folder1/folder2/encrypted' % REMOTE_UNIT_TEST_FOLDER,
-          'This is secret'], password='dummy')
-    assert main([
-        'ls',
-        '%s/folder1/folder2/encrypted' % REMOTE_UNIT_TEST_FOLDER],
-        verbose=0
-    )
+    # Create encrypted file
+    encrypted_file = '%sfolder1/folder2/encrypted' % REMOTE_UNIT_TEST_FOLDER
+    assert main(['-e', encrypted_file, 'This is secret'], password='dummy')
+    assert main(['ls', encrypted_file], verbose=0), \
+        "Creating encrypted file failed"
 
-    main(['mkdir', '%s/folder3/' % REMOTE_UNIT_TEST_FOLDER])
-    main(['mv', '%s/folder1/folder2/' % REMOTE_UNIT_TEST_FOLDER,
-          '%s/folder3/' % REMOTE_UNIT_TEST_FOLDER])
-    assert main([
-        'ls',
-        '%s/folder3/folder2/encrypted' % REMOTE_UNIT_TEST_FOLDER
-    ], verbose=0)
+    # Move folder with encrypted file
+    source_folder = '%sfolder1/folder2/' % REMOTE_UNIT_TEST_FOLDER
+    target_folder = '%sfolder3/' % REMOTE_UNIT_TEST_FOLDER
+    moved_encrypted_file = \
+        '%sfolder3/folder2/encrypted' % REMOTE_UNIT_TEST_FOLDER
+    assert main(['mkdir', target_folder])
+    assert main(['mv', source_folder, target_folder])
+    assert main(['ls', moved_encrypted_file], verbose=0), \
+        "Moving folder with encrypted file failed"
     print("Move encrypted %s" % green("OK"))
 
-    # Create an encrypted file and root and try to overwrite it
-    main(['-e', '%s/secret_file' % REMOTE_UNIT_TEST_FOLDER,
-          'Some encrypted data'],
-         password='pass')
-    assert main(['ls', '%s/secret_file' % REMOTE_UNIT_TEST_FOLDER], verbose=0)
-    assert not main(['-f', '%s/secret_file' % REMOTE_UNIT_TEST_FOLDER,
-                     'Here is some more content'])
-    print("Create encrypted in root %s" % green("OK"))
+    # Create an encrypted file and root and try to overwrite it as plain
+    encrypted_file2 = '%sencrypted2' % REMOTE_UNIT_TEST_FOLDER
+    assert main(
+        ['-e', encrypted_file2, 'Some encrypted data'],
+        password='pass'
+    )
+    assert main(['ls', encrypted_file2], verbose=0)
+    assert not main(['-f', encrypted_file2, 'Here is some more content'])
+    print("encrypted file plain text collision %s" % green("OK"))
 
     # Fail at creating folder with same name
-    assert not main(['mkdir', '%s/secret_file/' % REMOTE_UNIT_TEST_FOLDER])
+    assert not main(['mkdir', encrypted_file2 + '/']), "mkdir collision"
     print("Encrypted file folder collision %s" % green("OK"))
 
-    # Fail at moving a file overwriting another file
-    main(['mv', '%s/secret_file' % REMOTE_UNIT_TEST_FOLDER,
-          '%s/folder3/folder2/encrypted' % REMOTE_UNIT_TEST_FOLDER])
+    # Fail at moving a file overwriting an encrypted file
+    assert not main(['mv', encrypted_file2, moved_encrypted_file]), \
+        "move collision"
     print("Encrypted file moved file collision %s" % green("OK"))
-
-    # Cleanup
-    if main(['ls', '%s/' % REMOTE_UNIT_TEST_FOLDER], verbose=0):
-        main(['rm', '-R', '%s/' % REMOTE_UNIT_TEST_FOLDER])
 
 
 if __name__ == '__main__':
